e03a6d751eeea2d9f28faffe844a441227f2e3db
Anderson Santos da Silva <andersonfarin@gmail.com>
2022-06-02 17:57:50 -0300

    Kth largest element in array

--- 5_Recursion_Sorting_Hoares_QuickSelect/src/main/p1_kth_largest_element_in_an_array/KthLargestElementInAnArray.java
+++ 5_Recursion_Sorting_Hoares_QuickSelect/src/main/p1_kth_largest_element_in_an_array/KthLargestElementInAnArray.java
@@ -4,7 +4,73 @@ package p1_kth_largest_element_in_an_array;
  * https://leetcode.com/problems/kth-largest-element-in-an-array/
  */
 public class KthLargestElementInAnArray {
-    public int findKthLargest(int[] nums, int k) {
-        return 0;
+    public int findKthLargestQuickSort(int[] nums, int k) {
+        int indexToFind = nums.length - k;
+        quickSort(nums, 0, nums.length - 1);
+        return nums[indexToFind];
+    }
+
+    public int findKthLargestQuickSelect(int[] nums, int k) {
+        int indexToFind = nums.length - k;
+        return quickSelect(nums, 0, nums.length - 1, indexToFind);
+    }
+
+    private int quickSelect(int[] arr, int left, int right, int indexToFind) {
+        int partitionIndex = getPartition2(arr, left, right);
+        if (partitionIndex == indexToFind) {
+            return arr[partitionIndex];
+        } else if (indexToFind < partitionIndex) {
+            return quickSelect(arr, left, partitionIndex - 1, indexToFind);
+        } else {
+            return quickSelect(arr, partitionIndex + 1, right, indexToFind);
+        }
+    }
+
+    private void quickSort(int[] arr, int left, int right) {
+        int partitionIndex = getPartition(arr, left, right);
+        if (left < partitionIndex - 1) { // Sort left half
+            quickSort(arr, left, partitionIndex - 1);
+        }
+        if (partitionIndex < right) { // Sort right half
+            quickSort(arr, partitionIndex, right);
+        }
+    }
+
+    private int getPartition2(int[] arr, int left, int right) {
+        int i = left;
+        for (int j = left; j <= right; j++) {
+            if (arr[j] <= arr[right]) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        return i - 1;
+    }
+
+    private int getPartition(int[] arr, int left, int right) {
+        int pivot = arr[(left + right)/2]; // Pick pivot
+        while(left <= right) {
+            // Find element on left that should be on right
+            while (arr[left] < pivot) {
+                left++;
+            }
+            // Find element on right that should be on left
+            while (arr[right] > pivot) {
+                right--;
+            }
+            // Swap elements, and move left and right indices
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
--- 5_Recursion_Sorting_Hoares_QuickSelect/src/test/p1_kth_largest_element_in_an_array/KthLargestElementInAnArrayTest.java
+++ 5_Recursion_Sorting_Hoares_QuickSelect/src/test/p1_kth_largest_element_in_an_array/KthLargestElementInAnArrayTest.java
@@ -7,6 +7,17 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 class KthLargestElementInAnArrayTest {
 
     @Test
-    void findKthLargest() {
+    void findKthLargestQuickSort() {
+        KthLargestElementInAnArray kthLargestElementInAnArray = new KthLargestElementInAnArray();
+        assertThat(kthLargestElementInAnArray.findKthLargestQuickSort(new int[] {5,3,1,6,4,2}, 2)).isEqualTo(5);
+        assertThat(kthLargestElementInAnArray.findKthLargestQuickSort(new int[] {2,3,1,2,4,2}, 4)).isEqualTo(2);
+        assertThat(kthLargestElementInAnArray.findKthLargestQuickSort(new int[] {3}, 1)).isEqualTo(3);
+    }
+    @Test
+    void findKthLargestQuickSelect() {
+        KthLargestElementInAnArray kthLargestElementInAnArray = new KthLargestElementInAnArray();
+        assertThat(kthLargestElementInAnArray.findKthLargestQuickSelect(new int[] {5,3,1,6,4,2}, 2)).isEqualTo(5);
+        assertThat(kthLargestElementInAnArray.findKthLargestQuickSelect(new int[] {2,3,1,2,4,2}, 4)).isEqualTo(2);
+        assertThat(kthLargestElementInAnArray.findKthLargestQuickSelect(new int[] {3}, 1)).isEqualTo(3);
     }
 }
